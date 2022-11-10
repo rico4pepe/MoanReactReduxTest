8528c618358c68e3b2f97b5f02793515acf4a814
rico4pepe <rico4pepe@gmail.com>
2022-11-10 18:06:14 +0100

    Adding more columns and creating a simple hove effect

--- src/store/rooms.js
+++ src/store/rooms.js
@@ -42,5 +42,88 @@ export const MoanRooms = [
     lastOccupant: 'Lord Varys',
     uuid: 6519,
     balance: 320
+  },
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 6145,
+    address: "No 75 Egbe Way",
+    room: 8,
+    image: "img.jpg",
+    location: "Lagos, NG",
+    lastOccupant: 'Olanrewaju Ibidokun',
+    uuid: 7519,
+    balance: 500
+  },
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 6335,
+    address: "No 21 Lekki Pennisula",
+    room: 10,
+    image: "img.jpg",
+    location: "Lagos, NG",
+    lastOccupant: 'Gabriel Joshua',
+    uuid: 1519,
+    balance: 720
+  },
+
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 8435,
+    address: "No  13 Ajase Way",
+    room: 10,
+    image: "img.jpg",
+    location: "Lagos NG",
+    lastOccupant: 'Jane Doe',
+    uuid: 1919,
+    balance: 300
+  },
+
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 6435,
+    address: "No  12 Bloke Street",
+    room: 4,
+    image: "img.jpg",
+    location: "Los-Angeles, NY",
+    lastOccupant: 'Timmy Faz',
+    uuid: 1719,
+    balance: 920
+  },
+
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 9435,
+    address: "No  17 Charles Way",
+    room: 11,
+    image: "img.jpg",
+    location: "Los-Angeles, NY",
+    lastOccupant: 'Jermy Praise',
+    uuid: 2719,
+    balance: 1000
+  },
+
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 10135,
+    address: "No  17 Liverpool Way",
+    room: 13,
+    image: "img.jpg",
+    location: "Liverpool, UK",
+    lastOccupant: 'Big Joe',
+    uuid: 2519,
+    balance: 1500
+  },
+
+  {
+    moveOutDate: new Date().toDateString(),
+    id: 1135,
+    address: "No  17 Chicago Way",
+    room: 15,
+    image: "img.jpg",
+    location: "Chicago, Miami",
+    lastOccupant: 'Rucfus B',
+    uuid: 2719,
+    balance: 2500
   }
+
 ]
